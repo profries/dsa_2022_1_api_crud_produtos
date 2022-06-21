f5e169caa28cf9d9b8bfb1f503588e2b0bc2f4c6
Luis Henrique Ries <prof.ries@gmail.com>
2022-06-21 20:31:16 -0300

    Adicionado operacoes PUT e DELETE e req.body

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+
+//Inserir
+app.post('/api/produtos', (req, res) => {
+  const produto = req.body;
+  console.log('POST produtos', req.body);
+  const data = {};
+  data.msg = `Adicionando produto ${produto.nome}`;
+  //res.sendStatus(201);
+  res.status(201).json(data);
+})
+
+//Listar
 app.get('/api/produtos', (req, res) => {
   const data = {};
   data.msg = `Listando produtos`;
+  //res.set('Content-Type', 'application/json');
   res.json(data);
 })
 
+//BuscarPorId
 app.get('/api/produtos/:id', (req, res) => {
     const id = req.params.id;
     const data = {};
@@ -16,15 +32,27 @@ app.get('/api/produtos/:id', (req, res) => {
     res.json(data);
 })
 
-app.post('/api/produtos', (req, res) => {
-  console.log('POST produtos')
+
+
+//Atualizar
+app.put('/api/produtos/:id', (req, res) => {
+  const id = req.params.id;
+  const produto = req.body;
   const data = {};
-  data.msg = `Adicionando produto`;
-  //res.sendStatus(201);
-  res.status(201).json(data);
+  data.msg = `Atualizando produto ${produto.nome} com id=${id}`;
+  res.json(data);
+})
+
+//Deletar
+app.delete('/api/produtos/:id', (req, res) => {
+  const id = req.params.id;
+  const data = {};
+  data.msg = `Deletando produto com id=${id}`;
+  res.json(data);
 })
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
